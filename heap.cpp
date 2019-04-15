feeecd3d132332d18f29d0d583e872256abc5da2
himanshu777m <42205932+himanshu777m@users.noreply.github.com>
2019-04-15 16:24:07 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -153,11 +153,24 @@ class Heap{
 
 
 int main(){
-	Heap h(13);
+	Heap h(20);
 
-	for(int i=10;i>0;i--){
-		h.insert(i);	
-	}
+	h.insert(25);
+	h.insert(12);
+	h.insert(16);
+	h.insert(13);
+	h.insert(10);
+	h.insert(8);
+	h.insert(14);
+	h.insert(11);
+	h.insert(7);
+	h.insert(4);
+	h.insert(3);
+	h.insert(8);
+	h.insert(10);
+	h.insert(5);
+	h.insert(4);
+	h.insert(6);
 
 	h.display();
 	h.extractmin();
